9734816280daccaed32f69d97bd5cddcf0219d7f
AngelJosueCM <angelcernam15@gmail.com>
2023-04-03 01:14:18 -0500

    Avance

---
+++ src/views/HomeView.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="home">
+    <h1>Rick and Morty</h1>
+    <div class="content">
+      <div v-for="(character, index) in characters" :key="index">
+    <div>{{ character.name }}</div>
+    <img :src="character.image" alt="">
+    <div>{{ character.gender }}</div>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from 'axios';
+
+export default {
+  name: 'HomeView',
+  components: {
+  },
+  data(){
+    return{
+      characters: null
+    }
+
+  },
+  mounted(){
+    console.log('verificando')
+    this.getTodos();
+  },
+  methods: {
+    getTodos(){
+        axios.get('https://rickandmortyapi.com/api/character/')
+      .then(res => {
+        (this.characters = res.data.results)
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
+ }
+}
+</script>
+
+<style scoped>
+body{
+  background-color:black ;
+}
+.content{
+  display: grip;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr;
+  gap: 0px 10px;
+}
+</style>
